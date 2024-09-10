c0715a81f3ea69a7193620d8a7d02e4152f3ff95
nemo <n3m0o7@hotmail.com>
2024-09-10 08:44:41 +0200

    validate map

--- main.c
+++ main.c
@@ -25,39 +25,54 @@ void map_print(Map map)
     printf("%.3f ],\n]\n", MAPAT(map, len, len));
 }
 
+int map_valide(Map map)
+{
+    for (int y=0; y<map.len; y++) {
+        for (int x=0; x<map.len; x++) {
+            if (MAPAT(map, x, y) != MAPAT(map, y, x))
+                return 0;
+            if (x == y && MAPAT(map, x, y) != -1)
+                return 0;
+        }
+    }
+    return 1;
+}
+
 int main()
 {
     Map map;
     map.len = 5;
     map.poids = malloc(sizeof(*map.poids) * map.len * map.len);
 
-    MAPAT(map, 0, 0) = 1;
-    MAPAT(map, 1, 0) = 2;
-    MAPAT(map, 2, 0) = 3;
-    MAPAT(map, 3, 0) = 4;
+    MAPAT(map, 0, 0) = -1;
+    MAPAT(map, 1, 0) = 5;
+    MAPAT(map, 2, 0) = 7;
+    MAPAT(map, 3, 0) = 3;
     MAPAT(map, 4, 0) = 5;
-    MAPAT(map, 0, 1) = 6;
-    MAPAT(map, 1, 1) = 7;
-    MAPAT(map, 2, 1) = 8;
-    MAPAT(map, 3, 1) = 9;
-    MAPAT(map, 4, 1) = 10;
-    MAPAT(map, 0, 2) = 11;
-    MAPAT(map, 1, 2) = 12;
-    MAPAT(map, 2, 2) = 13;
-    MAPAT(map, 3, 2) = 14;
-    MAPAT(map, 4, 2) = 15;
-    MAPAT(map, 0, 3) = 16;
-    MAPAT(map, 1, 3) = 17;
-    MAPAT(map, 2, 3) = 18;
-    MAPAT(map, 3, 3) = 19;
-    MAPAT(map, 4, 3) = 20;
-    MAPAT(map, 0, 4) = 21;
-    MAPAT(map, 1, 4) = 22;
-    MAPAT(map, 2, 4) = 23;
-    MAPAT(map, 3, 4) = 24;
-    MAPAT(map, 4, 4) = 25;
+    MAPAT(map, 0, 1) = 5;
+    MAPAT(map, 1, 1) = -1;
+    MAPAT(map, 2, 1) = -1;
+    MAPAT(map, 3, 1) = -1;
+    MAPAT(map, 4, 1) = 9;
+    MAPAT(map, 0, 2) = 7;
+    MAPAT(map, 1, 2) = -1;
+    MAPAT(map, 2, 2) = -1;
+    MAPAT(map, 3, 2) = 2;
+    MAPAT(map, 4, 2) = -1;
+    MAPAT(map, 0, 3) = 3;
+    MAPAT(map, 1, 3) = -1;
+    MAPAT(map, 2, 3) = 2;
+    MAPAT(map, 3, 3) = -1;
+    MAPAT(map, 4, 3) = 1;
+    MAPAT(map, 0, 4) = 5;
+    MAPAT(map, 1, 4) = 9;
+    MAPAT(map, 2, 4) = -1;
+    MAPAT(map, 3, 4) = 1;
+    MAPAT(map, 4, 4) = -1;
 
     map_print(map);
+    if (!map_valide(map))
+        printf("map not valide\n");
 
     return 0;
 }
