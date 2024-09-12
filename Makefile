2c90dae31572503ff8b1372dc608da4c3b7dcae6
nemo <n3m0o7@hotmail.com>
2024-09-12 10:49:37 +0200

    map coord + map dist

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 all:
-	clang main.c -o algo-genetique -lraylib
+	clang -Wall -Wextra main.c -o algo-genetique -lraylib
 
 run: all
 	./algo-genetique
 
 win:
-	x86_64-w64-mingw32-gcc raylib-speedrun.c -o algo-genetique -L/usr/local/lib -I/usr/local/include -lraylib-win -lgdi32 -lwinmm
+	x86_64-w64-mingw32-gcc -Wall -Wextra raylib-speedrun.c -o algo-genetique -L/usr/local/lib -I/usr/local/include -lraylib-win -lgdi32 -lwinmm
--- main.c
+++ main.c
@@ -1,37 +1,74 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define MAPAT(map, x, y) ((map).poids[(x) + (y)*((map).len)])
+#include "raylib.h"
 
-typedef struct Map {
-    float *poids;
-    // char *names;
+#define MAPDISTAT(map, x, y) ((map).poids[(x) + (y)*((map).len)])
+
+#define MAPCOORDAT(map, x, y) ((map).coord[(x) + (y)*((map).len)])
+
+typedef struct MapCoord {
+    Vector2 *coord;
     int len;
-} Map;
+} MapCoord;
 
-void map_print(Map map)
+typedef struct MapDistance {
+    float *poids;
+    int len;
+} MapDistance;
+
+void map_distance_print(MapDistance map)
 {
     int len = map.len - 1;
     printf("[\n    [ ");
     for (int y=0; y<len; y++) {
         for (int x=0; x<len; x++) {
-            printf("%.3f, ", MAPAT(map, x, y));
+            printf("%.3f, ", MAPDISTAT(map, x, y));
         }
-        printf("%.3f ],\n    [ ", MAPAT(map, len, y));
+        printf("%.3f ],\n    [ ", MAPDISTAT(map, len, y));
     }
     for (int x=0; x<len; x++) {
-        printf("%.3f, ", MAPAT(map, x, len));
+        printf("%.3f, ", MAPDISTAT(map, x, len));
     }
-    printf("%.3f ],\n]\n", MAPAT(map, len, len));
+    printf("%.3f ],\n]\n", MAPDISTAT(map, len, len));
 }
 
-int map_valide(Map map)
+int map_distance_valide(MapDistance map)
 {
     for (int y=0; y<map.len; y++) {
         for (int x=0; x<map.len; x++) {
-            if (MAPAT(map, x, y) != MAPAT(map, y, x))
+            if (MAPDISTAT(map, x, y) != MAPDISTAT(map, y, x))
                 return 0;
-            if (x == y && MAPAT(map, x, y) != -1)
+            if (x == y && MAPDISTAT(map, x, y) != -1)
+                return 0;
+        }
+    }
+    return 1;
+}
+
+void map_coord_print(MapCoord map)
+{
+    int len = map.len - 1;
+    printf("[\n    [ ");
+    for (int y=0; y<len; y++) {
+        for (int x=0; x<len; x++) {
+            printf("(%.3f, %.3f), ", MAPCOORDAT(map, x, y).x, MAPCOORDAT(map, x, y).y);
+        }
+        printf("(%.3f, %.3f) ],\n    [ ", MAPCOORDAT(map, len, y).x, MAPCOORDAT(map, len, y).y);
+    }
+    for (int x=0; x<len; x++) {
+        printf("(%.3f, %.3f), ", MAPCOORDAT(map, x, len).x, MAPCOORDAT(map, x, len).y);
+    }
+    printf("(%.3f, %.3f) ],\n]\n", MAPCOORDAT(map, len, len).x, MAPCOORDAT(map, len, len).y);
+}
+
+int map_coord_valide(MapCoord map)
+{
+    for (int y=0; y<map.len; y++) {
+        for (int x=0; x<map.len; x++) {
+            float xx = MAPCOORDAT(map, x, y).x;
+            float yy = MAPCOORDAT(map, x, y).y;
+            if (xx < 0.0 || xx > 1.0 || yy < 0.0 || yy > 1.0)
                 return 0;
         }
     }
@@ -40,39 +77,25 @@ int map_valide(Map map)
 
 int main()
 {
-    Map map;
-    map.len = 5;
-    map.poids = malloc(sizeof(*map.poids) * map.len * map.len);
+    MapCoord mapc;
+    mapc.len = 5;
+    mapc.coord = malloc(sizeof(*mapc.coord) * mapc.len * mapc.len);
 
-    MAPAT(map, 0, 0) = -1;
-    MAPAT(map, 1, 0) = 5;
-    MAPAT(map, 2, 0) = 7;
-    MAPAT(map, 3, 0) = 3;
-    MAPAT(map, 4, 0) = 5;
-    MAPAT(map, 0, 1) = 5;
-    MAPAT(map, 1, 1) = -1;
-    MAPAT(map, 2, 1) = -1;
-    MAPAT(map, 3, 1) = -1;
-    MAPAT(map, 4, 1) = 9;
-    MAPAT(map, 0, 2) = 7;
-    MAPAT(map, 1, 2) = -1;
-    MAPAT(map, 2, 2) = -1;
-    MAPAT(map, 3, 2) = 2;
-    MAPAT(map, 4, 2) = -1;
-    MAPAT(map, 0, 3) = 3;
-    MAPAT(map, 1, 3) = -1;
-    MAPAT(map, 2, 3) = 2;
-    MAPAT(map, 3, 3) = -1;
-    MAPAT(map, 4, 3) = 1;
-    MAPAT(map, 0, 4) = 5;
-    MAPAT(map, 1, 4) = 9;
-    MAPAT(map, 2, 4) = -1;
-    MAPAT(map, 3, 4) = 1;
-    MAPAT(map, 4, 4) = -1;
+    MAPCOORDAT(mapc, 0, 0) = (Vector2) {0.12,  0.69};
+    MAPCOORDAT(mapc, 1, 0) = (Vector2) {0.1,   0.69};
+    MAPCOORDAT(mapc, 2, 0) = (Vector2) {0.2,   0.69};
+    MAPCOORDAT(mapc, 3, 0) = (Vector2) {0.42,  0.69};
+    MAPCOORDAT(mapc, 4, 0) = (Vector2) {0.4,   0.69};
+    MAPCOORDAT(mapc, 0, 1) = (Vector2) {0.2,   0.69};
+    MAPCOORDAT(mapc, 1, 1) = (Vector2) {0.69,  0.69};
+    MAPCOORDAT(mapc, 2, 1) = (Vector2) {0.6,   0.69};
+    MAPCOORDAT(mapc, 3, 1) = (Vector2) {0.9,   0.69};
+    MAPCOORDAT(mapc, 4, 1) = (Vector2) {0.412, 0.69};
 
-    map_print(map);
-    if (!map_valide(map))
-        printf("map not valide\n");
+    map_coord_print(mapc);
+
+    if (!map_coord_valide(mapc))
+        printf("mapc not valide\n");
 
     return 0;
 }
